7ef26d8e56f6839d776c9914b675337c7ffe6513
Rohan Ambhore <142285944+rohan1207@users.noreply.github.com>
2025-02-10 17:25:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 function firstChar(text) {
-  // your code here
+  // Trim leading and trailing spaces
+  text = text.trim();
 
-	if(text==' '){
-		return ' ';
-	}
-	else{
-		return text.trim().charAt(0);
-	}
+ 
+  if (text === "") {
+    return "";
+  }
+
+ 
+  return text.charAt(0);
 }
 
 // Do not change the code below
-//Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+// Uncomment the following line to show the prompt popup
+const text = prompt("Enter text:");
 alert(firstChar(text));
